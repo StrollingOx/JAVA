71e481bc82b7da8dc481206cc4e1396d486eb95b
Antoni Urbaniak <38950013+StrollingOx@users.noreply.github.com>
2018-10-02 09:02:10 +0200

    Update lab01_1.java

--- JAVAFX/lab01_1.java
+++ JAVAFX/lab01_1.java
@@ -6,7 +6,7 @@ import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
-public class Main extends Application
+public class lab01_1 extends Application
 {
 
     @Override
